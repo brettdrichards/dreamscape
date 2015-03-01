0e207444e2c012e736bcbf90d683daa1c1becf93
landonhaley <landonhaley@icloud.com>
2015-03-01 17:07:56 -0500

    assets folder

---
+++ Assets/movement.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var jumpH = 10;
+var isFalling = false;
+
+function Start () {
+
+}
+
+function Update () {
+
+	transform.position.z = 0;
+
+	 if (Input.GetKeyDown("w") && isFalling==false) {
+	 	rigidbody.velocity.y = jumpH;
+	 	isFalling = true;
+	 }
+	 if (Input.GetKeyDown("a")) {
+	 	transform.position.x -= 1.0;
+	 }
+	 if (Input.GetKeyDown("d")) {
+	 	transform.position.x += 1.0;
+	 }
+}
+
+function OnCollisionStay () {
+	isFalling = false;
+}
